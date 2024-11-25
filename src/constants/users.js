9bbf3df4b64ea6a08a431675f7eca15b8f1dd3a8
Vol3odya <Vol_odya@ukr.net>
2024-11-26 00:02:03 +0200

    finish1

--- src/constants/users.js
+++ src/constants/users.js
@@ -1,4 +1,5 @@
 export const emailRegexp =/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
+
 export const accessTokenLifetime = 900000;
 export const refreshTokenLifetime = 604800000;
